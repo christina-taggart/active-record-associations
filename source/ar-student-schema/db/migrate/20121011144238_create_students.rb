d392371d7a120c0bddad0334cd54be1311d5eaa7
Apprentice <apprentice@dbc27.local>
2014-01-22 20:33:51 -0800

    Completed Release 0

---
+++ source/ar-student-schema/db/migrate/20121011144238_create_students.rb
@@ -0,0 +1,15 @@
+require_relative '../config'
+
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.date :birthday
+      t.string :email
+      t.string :phone
+      t.timestamps
+    end
+  end
+end
