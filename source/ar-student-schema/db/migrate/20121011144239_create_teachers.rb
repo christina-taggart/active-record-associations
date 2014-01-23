8040292c74b3eeee6154886c1dae0de85f8fffa0
John Olmsted <null>
2014-01-22 16:56:08 -0800

    Add Teacher model and create_teachers migration

--- source/ar-student-schema/db/migrate/20121011144239_create_teachers.rb
+++ source/ar-student-schema/db/migrate/20121011144239_create_teachers.rb
@@ -1,16 +1,12 @@
 require_relative '../config'
 
-class CreateStudents < ActiveRecord::Migration
+class CreateTeachers < ActiveRecord::Migration
   def change
-    create_table :students do |col|
+    create_table :teachers do |col|
       col.string :first_name
       col.string :last_name
-      col.string :gender
-      col.date :birthday
       col.string :email
       col.string :phone
     end
   end
 end
-
-
